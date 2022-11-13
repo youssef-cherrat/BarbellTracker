f538c0166027f70f9422a40fdaea2e27a08624c2
youssef-cherrat <115671884+youssef-cherrat@users.noreply.github.com>
2022-11-13 17:10:05 -0500

    Add files via upload

---
+++ game.py
@@ -0,0 +1,25 @@
+#Youssef Cherrat - jja3em
+#Charles Melvin - jgm6jy
+
+
+#Description of Game
+#A metroid style game, where the user will control a sprite that can shoot enemies to advance and finish the level
+#The level will consist of stationary obstacles and a permanent roof and floor to prevent out of bounds.
+#A title screen to give the user a choice to choose the difficulty
+
+
+
+
+#3 basic features of game
+    #User Input Key binds: W = UP (character can go infinitly up with jetpack); A = Left movementd; D = Right movement, S = Crouch Feature
+        #Left Click = shoot/attack from character
+    #Game Over upon death of character: health bar point system, where if the character collides with the enemies a health point will deduct
+        #When health points are zero, display game over message with restart button
+    #Graphics/Images: 16 bit Sprite images of characteer and enemies, stationary obstacles images that restrict character movement
+
+#4 addtional features
+    #Multiple difficulty system: Easy = 3 health points, Medium = 2 health points, Hard = 1 health points
+    #Health Bar: Health Bar system, in which the character can start with a pre-inputted amount and enemies can subtract from the health point system
+    #Restart from Game Over: If character dies and runs out of health points, give user to respawn from the start
+    #Enemies: Spawned enemies that move towards user and die with one hit from user attack
+    #Sprite Animation: 16-bit character animation from attack input, movement animation for character based on up movement, enemy sprite dying animation
