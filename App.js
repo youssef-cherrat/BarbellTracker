8496007d781fa7199e218590a14593278c7cf0eb
youssef-cherrat <115671884+youssef-cherrat@users.noreply.github.com>
2024-06-10 19:58:21 -0400

    working on adding github linkage and icon

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import IntroPage from './pages/IntroPage';
-import DisplayPage from './pages/DisplayPage'; // Updated import to DisplayPage
+import DisplayPage from './pages/DisplayPage';
 import ExportPage from './pages/ExportPage';
 import EditVideoPage from './pages/EditVideoPage';
+import { TouchableOpacity } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Linking } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,7 +15,18 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Intro">
-        <Stack.Screen name="Intro" component={IntroPage} />
+        <Stack.Screen 
+          name="Intro" 
+          component={IntroPage} 
+          options={{
+            headerLeft: () => (
+              <TouchableOpacity onPress={() => Linking.openURL('https://github.com/youssef-cherrat/BarbellTracker')}>
+                <Ionicons name="logo-github" size={32} color="black" style={{ marginLeft: 10 }} />
+              </TouchableOpacity>
+            ),
+            title: 'Intro'
+          }} 
+        />
         <Stack.Screen 
           name="Upload" 
           component={DisplayPage} 
